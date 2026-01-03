3ccee7876fd78fc7747894bf0a087d686df2a58c
HeliseiKroopin <xursxidtojiyev@gmail.com>
2026-01-03 21:42:03 +0500

    all example file is ready

--- example/main.c
+++ example/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "../include/uplow.h"
 int main() {
-    printf("%d", is_upper('R'));
+    printf("Is Upper: %d\n", is_upper('F')); // 1 - true
+    printf("Is Lower: %d\n", is_lower('u')); // 1 - true
+    printf("To Upper: %c\n", to_upper('c')); // return - C
+    printf("To Lower: %c\n", to_lower('K')); // return - k
 }
